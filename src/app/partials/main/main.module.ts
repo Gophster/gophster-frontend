86c0ff573dfcd969641ab391de42936f603e61db
ArtZ23 <zzartur77@gmail.com>
2020-04-09 18:52:43 +0400

    Update main page

--- src/app/partials/main/main.module.ts
+++ src/app/partials/main/main.module.ts
@@ -1,20 +1,23 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-import { routes } from './main.routes';
-import {AuthGuard, NonAuthGuard} from '../../guards';
-import {MainComponent} from './main.component';
-import {DashboardModule} from './dashboard/dashboard.module';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { routes } from "./main.routes";
+import { AuthGuard, NonAuthGuard } from "../../guards";
+import { MainComponent } from "./main.component";
+import { DashboardModule } from "./dashboard/dashboard.module";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { TimelineComponent } from "./timeline/timeline.component";
 
 @NgModule({
-  imports: [CommonModule,
+  imports: [
+    CommonModule,
+    FormsModule,
     DashboardModule,
-    RouterModule.forChild(routes)],
-  declarations: [MainComponent],
+    RouterModule.forChild(routes),
+  ],
+  declarations: [MainComponent, SidebarComponent, TimelineComponent],
   exports: [MainComponent],
-  providers: [AuthGuard]
+  providers: [AuthGuard],
 })
-
-export class MainModule {
-}
+export class MainModule {}
