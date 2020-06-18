092f81339a1a80b97b3be877eb67d89595315e65
ArtZ23 <zzartur77@gmail.com>
2020-06-18 22:10:50 +0400

    Fix meesenger bugs and Add message button

--- src/app/partials/main/user/user.component.ts
+++ src/app/partials/main/user/user.component.ts
@@ -4,7 +4,7 @@ import { UserService } from '../../../services/user/user.service';
 import { Subscription } from 'rxjs';
 import Swal from 'sweetalert2';
 import { GophsService } from '../../../services/gophs/gophs.service';
-import {ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 declare var $: any;
 
 @Component({
@@ -27,7 +27,16 @@ export class UserComponent implements OnInit, OnDestroy {
     fileName: string;
     followersAmount: number;
     followingAmount: number;
-  } = { handle: '', name: '', avatar: null, fileName: '', birthdate: '', location: '', followersAmount: null, followingAmount: null };
+  } = {
+    handle: '',
+    name: '',
+    avatar: null,
+    fileName: '',
+    birthdate: '',
+    location: '',
+    followersAmount: null,
+    followingAmount: null,
+  };
   public obj: {
     handle: string;
     name: string;
@@ -37,7 +46,16 @@ export class UserComponent implements OnInit, OnDestroy {
     fileName: string;
     followersAmount: number;
     followingAmount: number;
-  } = { handle: '', name: '', avatar: null, fileName: '', birthdate: '', location: '', followersAmount: null, followingAmount: null,  };
+  } = {
+    handle: '',
+    name: '',
+    avatar: null,
+    fileName: '',
+    birthdate: '',
+    location: '',
+    followersAmount: null,
+    followingAmount: null,
+  };
   public followButton: boolean;
   public birthDate: string;
   public mainHandle: string;
@@ -155,41 +173,52 @@ export class UserComponent implements OnInit, OnDestroy {
   }
 
   public onFollow() {
-    const sendHandle = { handle : this.user.handle};
-    this.followUserSubscription = this.userService.followUser(sendHandle).subscribe((response) => {
-      this.getUserData(this.routeParam);
-      this.isFollowing();
-    });
+    const sendHandle = { handle: this.user.handle };
+    this.followUserSubscription = this.userService
+      .followUser(sendHandle)
+      .subscribe((response) => {
+        this.getUserData(this.routeParam);
+        this.isFollowing();
+      });
   }
 
   public onUnfollow() {
-    const sendHandle = { handle : this.user.handle};
-    this.unfollowUserSubscription = this.userService.unfollowUser(sendHandle).subscribe((response) => {
-      this.getUserData(this.routeParam);
-      this.isFollowing();
-    });
+    const sendHandle = { handle: this.user.handle };
+    this.unfollowUserSubscription = this.userService
+      .unfollowUser(sendHandle)
+      .subscribe((response) => {
+        this.getUserData(this.routeParam);
+        this.isFollowing();
+      });
   }
 
   public isFollowing() {
-    const sendHandle = { handle : this.user.handle};
-    this.isFollowingSubscription = this.userService.isFollowing(sendHandle).subscribe((response) => {
-      this.followButton = response.data;
-    }, (error) => {
-      this.router.navigate(['/404']);
-    });
+    const sendHandle = { handle: this.user.handle };
+    this.isFollowingSubscription = this.userService
+      .isFollowing(sendHandle)
+      .subscribe(
+        (response) => {
+          this.followButton = response.data;
+        },
+        (error) => {
+          this.router.navigate(['/404']);
+        }
+      );
   }
 
   public deleteGoph(item: any) {
-    this.deleteGophSubscription = this.gophsService.deleteGoph(item.id).subscribe((response) => {
-      this.posts = this.posts.filter(goph => goph.id !== item.id);
-      Swal.fire({
-        title: '',
-        text: 'The Goph has successfully been deleted',
-        icon: 'success',
-        confirmButtonColor: 'rgb(171, 119, 75)',
-        timer: 3000,
+    this.deleteGophSubscription = this.gophsService
+      .deleteGoph(item.id)
+      .subscribe((response) => {
+        this.posts = this.posts.filter((goph) => goph.id !== item.id);
+        Swal.fire({
+          title: '',
+          text: 'The Goph has successfully been deleted',
+          icon: 'success',
+          confirmButtonColor: 'rgb(171, 119, 75)',
+          timer: 3000,
+        });
       });
-    });
   }
 
   public editGoph(index: number) {
@@ -198,20 +227,22 @@ export class UserComponent implements OnInit, OnDestroy {
 
   public onEditGoph(editedItem: any, index: number) {
     const sendObject = {
-      text: editedItem.text
+      text: editedItem.text,
     };
-    this.editGophSubscription = this.gophsService.editGoph(editedItem.id, sendObject).subscribe((response) => {
-      const editedGoph = this.posts.find(item => item.id === response.id);
-      editedGoph.text = response.text;
-      this.cancelEditGoph(index);
-      Swal.fire({
-        title: '',
-        text: 'The Goph has successfully updated',
-        icon: 'success',
-        confirmButtonColor: 'rgb(171, 119, 75)',
-        timer: 3000,
+    this.editGophSubscription = this.gophsService
+      .editGoph(editedItem.id, sendObject)
+      .subscribe((response) => {
+        const editedGoph = this.posts.find((item) => item.id === response.id);
+        editedGoph.text = response.text;
+        this.cancelEditGoph(index);
+        Swal.fire({
+          title: '',
+          text: 'The Goph has successfully updated',
+          icon: 'success',
+          confirmButtonColor: 'rgb(171, 119, 75)',
+          timer: 3000,
+        });
       });
-    });
   }
 
   public cancelEditGoph(index: number) {
@@ -222,12 +253,20 @@ export class UserComponent implements OnInit, OnDestroy {
     history.scrollRestoration = 'manual';
     if (this.queryParams.currentPage <= this.queryParams.totalPages) {
       if (this.routeParam) {
-        this.getProfileGoph(this.routeParam, `?page=${this.queryParams.currentPage}`);
+        this.getProfileGoph(
+          this.routeParam,
+          `?page=${this.queryParams.currentPage}`
+        );
       } else {
-        this.getProfileGoph(this.mainHandle, `?page=${this.queryParams.currentPage}`);
+        this.getProfileGoph(
+          this.mainHandle,
+          `?page=${this.queryParams.currentPage}`
+        );
       }
       this.queryParams.currentPage++;
     }
   }
-
+  public onItemClick(userId: number) {
+    this.router.navigate([`/message/${userId}`]);
+  }
 }
